3bf4cacc9d5de5324239bc9e4f55913b3692060a
Sebastian Albert <salbert2000@googlemail.com>
2020-09-27 19:47:57 +0200

    Hi2

--- Merging/Assets/PlayerManager.cs
+++ Merging/Assets/PlayerManager.cs
@@ -7,7 +7,7 @@ public class PlayerManager : MonoBehaviour
     private int gold;
     private int diamants;
     private int level;
-
+    // Bastis Text ist hier, der ist aber unwichtig im Vergleich zu Schatzis :D
     private int getGold() { return gold; }
 
     private int getDiamants() { return diamants; }
