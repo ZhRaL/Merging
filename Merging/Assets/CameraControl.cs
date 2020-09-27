688b7fcb317140ed52506ca7e5d8931177e442a7
Sebastian Albert <salbert2000@googlemail.com>
2020-09-27 19:44:16 +0200

    Combi Test mit Camera

---
+++ Merging/Assets/CameraControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private Camera camera;
+
+    private Vector3 touchStart;
+    private float zoomOutMin=10;
+    private float zoomOutmax=80;
+
+    void Start()
+    {
+        camera = Camera.main;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            touchStart = camera.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if(Input.touchCount==2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            zoom(difference * 0.01f);
+        }else if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Here");
+            Vector3 direction = touchStart - camera.ScreenToWorldPoint(Input.mousePosition);
+            camera.transform.position += direction;
+        }
+
+    }
+    void zoom(float increment)
+    {
+        camera.orthographicSize = Mathf.Clamp(camera.orthographicSize - increment, zoomOutMin, zoomOutmax);
+    }
+}
---
+++ Merging/Assets/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private int gold;
+    private int diamants;
+    private int level;
+
+    private int getGold() { return gold; }
+
+    private int getDiamants() { return diamants; }
+
+    private int getLevel() { return level; }
+
+    private void raiseLevel() { level++; }
+
+    private void setLevel(int i) { level = i;}
+
+    private void setGold(int i) { gold = i; }
+    private void setDiamants(int i) { diamants = i; }
+}
